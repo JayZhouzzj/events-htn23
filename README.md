ea6be6adf1f3e4ae0faeb3c3dae183d4b4f6a518
Zhengjie Zhou (Jay) <zhengjiezhou@dal.ca>
2023-02-22 22:19:42 -0500

    Nit

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 This is a submission to the frontend developer role at Hack the North 2023
 
 ### How to run
+```npm install```  
+```npm run dev```
 
 ### Writeup
+See docs/writeup.pdf
 
 ### Built with Vue 3 + Vite
--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -26,7 +26,7 @@ function getEvent(id, events) {
         <p class="mt-2 text-lg leading-8 text-gray-600 text-left">Hack the North</p>
       </div>
       <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-        <article v-for="event in events" :key="event.id" class="flex max-w-xl flex-col items-start justify-between">
+        <article v-for="event in events" :key="event.id" class="flex max-w-xl flex-col items-start justify-start">
           <div class="flex items-center gap-x-4 text-xs">
             <time :datetime="event.start_time" class="text-gray-500">{{ 
               new Date(event.start_time).toLocaleTimeString(undefined, {
@@ -50,8 +50,8 @@ function getEvent(id, events) {
             </h3>
             <p class="mt-5 text-sm leading-6 text-gray-600 line-clamp-3 text-left">{{ event.description }}</p>
           </div>
-          <div class="relative mt-8 flex gap-x-4">
-            <div class="text-sm leading-6">
+          <div class="relative flex gap-x-4">
+            <div class="mt-5 text-sm leading-6">
               <p class="font-semibold text-gray-900 text-left">
                 {{ event.speakers.length > 1 ? "Speakers:" : "Speaker: "}}
                 <span v-for="(speaker, index) in event.speakers" :key="speaker.name">
@@ -65,8 +65,8 @@ function getEvent(id, events) {
                 Related: 
                 <br />
                 <span v-for="id in event.related_events" :key="id" class="text-left">
-                  <a :href="getEvent(id, events)?.public_url" class="relative z-10 inline-block rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100 text-ellipsis ml-0 mr-2 my-0.5 text-xs" rel="noopener noreferrer" target="_blank">{{
-                    getEvent(id, events)?.name
+                  <a v-if="getEvent(id, events)" :href="getEvent(id, events).public_url" class="relative z-10 inline-block rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100 text-ellipsis ml-0 mr-2 my-0.5 text-xs" rel="noopener noreferrer" target="_blank">{{
+                    getEvent(id, events).name
                   }}</a>
                 </span>
               </p>
