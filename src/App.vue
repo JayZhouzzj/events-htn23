07d00ceda4129ad4b3fe2c1468720613752a07d5
Zhengjie Zhou (Jay) <zhengjiezhou@dal.ca>
2023-02-22 19:27:50 -0500

    GraphQL connected

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <script setup>
+import { useQuery } from '@vue/apollo-composable';
+import gql from 'graphql-tag';
 import EventList from './components/EventList.vue'
+
+const EVENTS_QUERY = gql`
+  query {
+    sampleEvents {
+      id
+      name
+      event_type
+      permission
+      start_time
+      end_time
+      description
+      speakers {
+        name
+      }
+      public_url
+      private_url
+      related_events
+    }
+  }
+`
+
+const { result, loading, error } = useQuery(EVENTS_QUERY)
 </script>
 
 <template>
-  <EventList/>
+  <div v-if="loading">Loading...</div>
+  <div v-else-if="error">{{ error.message }}</div>
+  <div v-else-if="result">
+    <EventList :events="result.sampleEvents" />
+  </div>
 </template>
 
 <style scoped>
